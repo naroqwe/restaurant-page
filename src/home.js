041cd00995cdbcc998f9cffbc4ca1a70bd3e8a5f
naroqwe <49810638+naroqwe@users.noreply.github.com>
2023-11-26 12:28:14 +0100

    created home page

---
+++ src/home.js
@@ -0,0 +1,22 @@
+function createHomePage() {
+  const content = document.querySelector("#content");
+
+  const menuPage = document.createElement("div");
+  const img = document.createElement("img");
+  const paragraph = document.createElement("p");
+
+  paragraph.innerHTML = `"We make the best pizza"`;
+
+  img.src = "/dist/images/chef.jpg";
+
+  img.classList.add("load-img");
+  paragraph.classList.add("load-paragraph");
+  menuPage.classList.add("load-menu-page");
+  content.classList.add("content-homepage");
+
+  content.appendChild(menuPage);
+  menuPage.appendChild(paragraph);
+  menuPage.appendChild(img);
+}
+
+export default createHomePage;
