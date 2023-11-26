cf987242938b8c5160ecba610b83b246c906dabf
naroqwe <49810638+naroqwe@users.noreply.github.com>
2023-11-26 12:53:33 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import createHomePage from "./home";
 
 window.addEventListener("load", createHomePage);
+
+let btns = ["home-tab", "menu-tab", "contact-tab"];
+
+btns.forEach(button, () => {
+  let btn = document.getElementsByClassName(button);
+  btn.addEventListener("click", () => {
+    console.log(btn);
+  });
+});
+
+// createHomePage();
+
+const homeTab = document.querySelector(".home-tab");
