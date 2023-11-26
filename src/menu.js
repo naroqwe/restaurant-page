ef195d3dcc0be1b92f0786f4c253a2d07d021921
naroqwe <49810638+naroqwe@users.noreply.github.com>
2023-11-26 17:53:13 +0100

    added menu page

--- src/menu.js
+++ src/menu.js
@@ -1,1 +1,36 @@
-function createMenu() {}
+function createMenu() {
+  const content = document.querySelector("#content");
+  const images = [
+    "/dist/images/california-pizza.jpg",
+    "/dist/images/chicago-pizza.jpg",
+    "/dist/images/detroit-pizza.jpg",
+    "/dist/images/miami-cuban-pizza.jpg",
+    "/dist/images/milan-style.jpg",
+    "/dist/images/new-york-pizza.jpg",
+  ];
+  const descriptions = [
+    "California-style Pizza",
+    "Chicago Deep-Dish Pizza",
+    "Detroit-style Pizza",
+    "Miami / Cuban Pizza",
+    "Milan-style Pizza",
+    "New York-style Pizza",
+  ];
+  for (let i = 0; i < 6; i++) {
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+    const desc = document.createElement("p");
+
+    img.src = images[i];
+    desc.innerHTML = descriptions[i];
+
+    content.appendChild(div);
+    div.appendChild(img);
+    div.appendChild(desc);
+
+    content.classList.add("content-menu");
+    div.classList.add("menu-load");
+  }
+}
+
+export default createMenu;
